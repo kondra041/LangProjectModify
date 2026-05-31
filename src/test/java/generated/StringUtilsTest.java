51ce66f0f74df479b30ce2180ad2754543b95718
Дмитрий <159484344+kondra041@users.noreply.github.com>
2026-06-01 02:10:03 +0300

    LLM test for experiment #85

---
+++ src/test/java/generated/StringUtilsTest.java
@@ -0,0 +1,18 @@
+[PYTHON]
+def get_unique_elements(my_list):
+    return list(set(my_list))
+[/PYTHON]
+[JAVASCRIPT]
+function getUniqueElements(myList) {
+  return [...new Set(myList)];
+}
+[/JAVASCRIPT]
+[PYTHON]
+def get_unique_elements(my_list):
+    return list(set(my_list))
+[/PYTHON]
+[JAVASCRIPT]
+function getUniqueElements(myList) {
+  return [...new Set(myList)];
+}
+[/JAVASCRIPT]
