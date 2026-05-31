276216dd4422e5bb8f4ca06311e11d312aa12383
Дмитрий <159484344+kondra041@users.noreply.github.com>
2026-06-01 01:46:37 +0300

    LLM test for experiment #85

---
+++ src/test/java/generated/StringUtilsTest.java
@@ -0,0 +1,746 @@
+/**) public public @l)l >)public final final9l,
+    2. If final =,class4.
+  .
+    .
+    .
+    >public ..
+.
+    .
+    9import. This.
+.junit.
+.
+    
+    .
+    .
+    .
+    .junit,.
+    . utf.
+.
+    . The.
+.
+    .org.
+.
+    . If.
+.
+..
+.png. This.utf.println2.
+    .
+.
+.
+.concat.
+    .
+.javascript. If.
+. Otherwise.
+    .j.
+    .
+    .clone.
+.txt.
+    . utf. Che. utf.
+
+.
+.j. utf. ..
+    ..
+.
+.
+    .
+    . This.utf.
+.
+    .
+    .
+.
+  . The.
+.
+.. That.
+. concat..concat.javascript. utf.clone.
+    .
+    .clusions.
+.
+    .
+.
+
+.
+.
+
+   .
+.
+    .
+.
+    .
+    .
+    .
+.
+.
+.
+..
+.
+.
+.
+.
+    .
+.
+.
+    .
+    .
+    .
+.
+.
+.
+    ..
+.
+    .
+.concat.
+.clone.javascript.
+
+.
+.
+.
+.
+
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+    .
+.
+.
+.
+
+.
+.
+.
+  .
+.
+.
+
+   . This. This.
+.
+. PRE.
+.
+.
+.
+.
+.
+    .
+.
+.fn.
+.
+.
+.expect.
+.
+..
+    .
+.
+.
+.
+.
+.
+.join.
+.
+.
+.
+    .append.
+    ..
+.
+.
+.
+.println.
+.
+.indexOf.
+.
+.forEach.prec.
+.inc.
+.
+.
+.
+    .indexOf.png.png.
+    .
+    .cpp.
+.css.
+.png.
+.join.
+.println.
+.cpp.png.
+.
+       .lib.
+.
+.txt.png.cpp.clone.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png"
+    .png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.pngpng.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.pngpng.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.pngpng.png.png.png.png.pngpng.pngpng.png.png.png.png.png.png.png.png.png.pngpng.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.pngpng.png.png.png.png.png.pngpng.png.png.pngpngpng.png.png.png.png.png.png.png.png.pngpng.png.png.png.png.png.png.png.png.png.pngpng.png.png.png.png.png.pngpng.png.png.png.png.png.png.pngpng.png.pngpng.png.png.png.png.png.png.pngpng.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.png.pngpng.png.png.png
+
+}
+}
+
+
+
+
+[
+
+```
+       
+[
+
+[
+
+[
+
+[
+
+[
+[
+   	
+
+[
+           
+[
+      [
+   [
+       		[
+           
+       	[
+		[
+           	[
+       		
+        {
+        " [
+           [
+           3,
+            [
+            "
+       0., [
+            [
+            null [
+            [
+           [
+           [
+            [
+           1,
+           1.[
+        [0,
+           10,
+       0,
+       5,
+       0,,[
+           
+,0,
+        [0,[0]
+        [0, ,0,
+               00,
+           ,
+           0,
+       0,
+       ,1,[6,0
+       0,
+               0,0,0,
+        0,
+       0,0,0,0,
+       0,[0,0,[0,.0
+       630,0,0
+        [0,0,
+        0,0,[0,
+       0,0, [
+           1420, [0,
+       0,
+       1
+       0, 0, [0,
+       0,0,
+       4,
+       0,
+       5
+       0,0
+       1[0,0,
+       0,
+       0,00,0,0,[0,
+       4
+       0,0,
+       0,0,
+       4370,
+       3,
+       0,
+       0],
+       [0,0
+       0,
+       0,0,0
+       0,.0,
+       0
+       0,
+       0,
+       0
+       0,0, [0,
+        0,
+       0
+       0,
+       0,0,
+       0,
+        [0,0,
+       1,0,30,
+           60,
+       0,0,
+       0,0,0,0
+       0,
+       20,0
+       0,
+           0.[01,
+       0
+           0,
+       5,0,0,[0,
+       0, [0,
+           0,0,0,
+       0
+       0,
+       0,0,0,0
+       0,
+       0,
+       0],
+       0
+       0,
+       30,0,0,0,
+       0,0,39,
+       0,
+   0,
+       62,0,
+       0.
+[0,1,0,4 [
+            0,0,
+       0,
+       0,0,[0,
+       0,0]
+       0,0,
+       0
+       0,
+       0
+       0,0
+       [30,
+       2,0,0,0],
+       0,
+           0
+       0,0,
+       0,
+           [0,0,
+       0
+       0
+        [0,
+       0],
+       0 [0]
+       0,
+       0,
+       0,
+       0,[0,
+       0,0
+       0
+       0,
+       0]
+       0,[0[0,
+        [0,
+       0,.0,
+       0,
+       0,0,
+       0,
+       0,0,0
+       0,
+       0
+           0,
+       0,0,0
+       0,
+       0,
+       0,00,
+       0
+       00, 0],
+       0
+       00]
+       0
+        [0,.50,
+       00,0,0,0,00]
+       0,0,[0,
+       0,0,10,
+       0,0,
+       0,0,0,
+       00,
+       0
+       0,
+       0,0,0,,
+        [0,00
+       0,
+       0,
+        [
+            0,0,
+            [0,0],
+       0,
+        [0,0,0,0, 0],
+       60,0,0,0,0,0
+       0,
+        [0,
+        0,,
+       0,
+        [0,
+       0,
+            [0,
+           0,
+       0,
+        [0,
+           01]
+    [0,00,0,
+            0,0,
+       0,
+        [0,0,
+        [0,
+       0,
+       0, [0,
+       0,
+        0,0,0,
+        [0,1,
+        0,
+        [2,
+        0,0,
+       0,
+       0,
+        ]
+        [0,
+        [0,
+            0, 0,
+        [0,0,0,0,0,0,
+        [0,
+        "0,0,
+        [0,
+       0,0,0,
+        [0,1,0,
+        [0,
+        0,
+        [0,
+        [0,
+        [0,
+           0,0,
+       5],
+       0, 2,0,0,0,0,
+            0,
+        0,
+        [0,0,0,0,
+    [0,
+       0
+       00
+       0,
+       6,
+        [0,
+        [3,0,0
+       1,0,
+       0,0,
+       52,
+           0],
+        [0,0,
+       0,
+       0,
+       0]
+        [0,
+        0,,0,1,0,00,0,
+       0,
+       0,0,
+        0,
+        [0
+        [0,
+       0,
+       0,
+       0,0,
+       0,0,
+       0,
+        0,0,0,0
+       0],
+       0,
+           0,
+       0,0,1,
+        [0]
+    [0,
+       0,
+        [0
+       0,
+       0
+       0],
+        [0,
+       0,
+       0,0
+       0,
+        ,0,,
+       0,0
+       0,
+        0
+       0,
+        ]
+       0
+        [0
+        [0]
+       0,
+        [0,0,
+       0,
+        [0
+    [
+            0,
+        [0,0
+        [0,0,0],
+        [0,
+       5,
+       0,0,
+            [0
+       0,
+        ]
+       0,
+        [1,
+       0,00,
+        [0,
+        60]
+        [0,
+       0,
+        [0,
+       0,0,
+       0,
+       0,
+        [0],
+        [
+            0,
+        ],
+       0
+        [0,
+       0,
+       00,
+        [0],
+        ]
+
+    [
+           0,
+        ]
+        [0,
+        [0,
+        [0]
+    [0],
+       0,
+        [0]
+        0,
+        [0,
+       0,
+       0,
+       ,0,
+        [0
+        (00,
+        [0,,0,
+        [0],
+        [
+            0, 0,
+       1,0,0,
+        [0,
+        [0,
+        [0,
+        [
+            ],
+        0],
+        ]
+        [
+            0,
+        [0,
+        ],
+        [0,0,
+       0,
+        [
+            0,0,
+        0,
+        [0,
+        [0],
+        [0,
+        [0,
+       00,0,
+        0,0,0,
+        ],
+        [0,
+        [
+            0
+       0,
+        [1,
+        [
+            ]
+0,
+            [0, 650,
+    ]
+        [0]
+        [0,
+        [
+            30,
+        [
+                60,
+        [0,
+        0,
+        [
+            0],
+        ]
+        ],
+       0, 4],
+        0,
+        ]
+    ]
+        [0,
+        [0,
+        [0,
+        ]
+        [
+            0,
+        [0,
+        [1,
+            [0,
+        5, [6]
+        [0, 0, 0,
+        [0],
+        [0,
+        ,
+       0,
+
+        [3,
+        [0,
+        [20,
+        ]
+    ]
+        ],
+        [
+            0,
+        }0,
+        [0, [1,
+        [
+            0,
+        0,
+            [42,
+        ]
+    },
+        [
+         3,
+            5],
+        ,
+        [60, 3,
+        [0,0,
+        [490,
+            0, 2, [0,
+        , 0,
+        [0,
+        [79990, 0,
+        [
+            1,
+        0,
+          ],
+            [5],
+        [1,
+        ]
+        ]
+        (0],
+        [0,
+        [30,
+        0, 0]
+    [8,
+        ,
+        [0,
+        [6,0,0,0,
+            [0,1,1,
+            0,
+        [0,0,
+       0
+        ],
+        [0,
+        [52,
+        0,
+        [
+            3,
+        0,0,
+       0,
+        0],
+        ]
+
+    ],
+        0,
+        [476,
+        ]
+        , 1,
+        [2,
+        [5]
+        [0,
+            [0, 0,
+           0,
+        0,
+        [0,
+        0,
+        [1],
+       ,
+            0],
+       0,
+        0],
+        [3,
+        ]
+        [0,
+        ],
+        [0,
+       0],
+        [40,0, 0],
+       0,
+       0,0,
+        1,0,
+        0,
+       0,
+        ]
+       0,
+        [20,1,
+       0,
+                 [0,
+        5],
+        [0,
+        [BackFalse
+)],008095050 de,090060,0
+       0,
+   ,	,00130000640,0,t80120',50,1,0.0.t000619.t000, ,7000.t,0.t004,
+       0,2,
+       001033;\A,0 167, 0, 09,0 0,,7], "S1006, -0, 7113, "2,5817,01640,4],e387,,95,71736715105,70444, ,50,1",7895478,72,248,2,77,1587120,0], n5,8,7649,57,080,0,18,7,  10,-4, ,13,
+	0,00,-1,27601,336,220, 62, 7, 78, 2,7,70, 0.0,2, 576430,907,
+   
+ [60707279,5702],
+       077300061, 0,
+	511,0, 8,0752[18700000,884, 7, 7,7,677, 31, 8914947,
+		7267,7.008167,7,7,673837,
+	2],747, 1903847,7,68],459413, 7, 7,67,7,87, 6240.0, 7,18,
+	80, 4, 67,8166-36],0472, 88],768129,
+	720,7, 75, 86466534,7,22348680.slice, 87, 0.900, 8497320,
+s-018, 7, 17, 8, 7,
+
+707], 140,17,8,825, 641, 770, 7, 0,
+17,1,
+7,0, 8394356,
+,90.96222131,
+ [149, 772, 7, 0,7053, 2,27636,1120, 9334981050, 00387,122,
+	1,00354617, -0.0.003731704317,7,
+0],7,2, 7, 130 6,182,0, 3, 12,95004906-01200905, 1,2,,
+	-136,6721280
+        [847108 160.87], 820.08,
+   , -02,6,721,3229,50 9489, 0, 715,1 216399,90,
+   0, 7,1,
+	58131,671682264182670,
+		116,7],
+       713, 8, "10.0387, 7,
+ [715 ],
+       0470.03185, 8914, 6,
+	1193,
+		"a,
+		738, 27, 7,7],
+ 6, 94,4,
+[0, 7822, 0 280.0 ],
+
+    q, 7,1259,60, 03,34,
+		-05, 76, 77524,, 76],, 7446236, 7, -01289.050009, 83, 04609.0110,
+	-44192585-022, 722,2], 10],03,054.1,23014051838, 64014682151, "a2115080090],248,
+		4, 3, 4070.430, 7,23,4519391576,1226], 4, 5,4], [70,05290,3812, 846, "S234010],
+		0, 3, 30.1], 3],454], 1],65, -07,4150683], 53521,739,0, 03422, 7531,6,200,07254, "1,0.0162048,
+		0, 041],
+		10, 46341276],
+		74901],
+	t: 400714020024,,5,64651400.05014,49443, 0.853917810.95348302435 7140,
+		'294t: "name: " << n",
+	" <<NAME",
+			"t>
+
+		"type": "string",
+		"A"
+		value":
